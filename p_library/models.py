dc9348677242f12913d3df7cfdb33a0cee9e15c5
Nikolai Vetrov <nvetrov@gmail.com>
2020-02-08 17:12:33 +0300

    D5.9 Формсеты Давайте теперь сделаем так, чтобы можно было создавать сразу несколько авторов и книг. Для такой задачи в Django есть Formstes (формсеты). Эти абстракции позволяют работать со множеством форм на одной странице.
    Для того чтобы создать формсет, нужно взять уже готовую форму (AuthorForm) и передать её в качестве параметра фабрике формсетов.
    
    Фабрика классов (в нашем случае формсетов) — это один из шаблонов проектирования, который применяется для создания объектов различных типов одним интерфейсом.

---
+++ p_library/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Friend(models.Model):
+    name = models.TextField(verbose_name='Кому одолжил(Имя)')
+    borrowed = models.DateField(verbose_name='Дата выдачи книги', null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    full_name = models.TextField()
+    birth_year = models.SmallIntegerField()
+    country = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Book(models.Model):
+    ISBN = models.CharField(max_length=13)
+    title = models.TextField()
+    description = models.TextField()
+    year_release = models.SmallIntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    friend = models.ForeignKey(Friend, on_delete=models.CASCADE)
+
